ebb9f916485408abe9b31fcfbaea170dc1a753e4
Madhan2407 <madhankumar070406@gmail.com>
2025-08-22 14:52:33 +0530

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -33,8 +33,8 @@ const projects: Project[] = [
     detail: 'A personal portfolio website showcasing projects, skills, and experience with a modern responsive design, smooth navigation, and clean UI/UX for a professional online presence.',
     techTags: ['React', 'Tailwind', 'Responsive Web Design'],
     imageUrl: 'https://i.pinimg.com/736x/a9/3f/08/a93f08415c4d2b1127b5b6780780cf5b.jpg',
-    repoUrl: 'https://github.com/Madhan2407/Portfolio/tree/main/MadhanKumar_Portfolio',
-    demoUrl: 'https://github.com/Madhan2407/Portfolio/tree/main/MadhanKumar_Portfolio',
+    repoUrl: 'http://github.com/Madhan2407/Portfolio',
+    demoUrl: 'http://github.com/Madhan2407/Portfolio',
     category: 'Web Development'
   },
   {
@@ -331,5 +331,6 @@ const Projects: React.FC = () => {
     </section>
   );
 };
+
 
 export default Projects;
